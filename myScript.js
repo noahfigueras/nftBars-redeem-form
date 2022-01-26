34bbb74236355ec095dc90c764459ec909ea3754
noah figueras <noahfigueras@gmail.com>
2022-01-25 22:05:45 -0800

    Fix Bug: Send user address only once

--- myScript.js
+++ myScript.js
@@ -37,6 +37,12 @@ async function populateForm() {
 	const tokens = await tokensToRedeem();
 	const addr = "0x5e079B2f1D98E07B4D53A5D0f6Ef5843235e8E15";// await signer.getAddress();
 	const ul = document.querySelector("#add-token");		
+	// Add address to form
+	const form = document.querySelector("#redeemForm");		
+	const input = document.createElement("input");
+	input.setAttribute("type", "hidden");
+	input.setAttribute("value", addr);
+	form.append(input);
 	// Populate user owned nfts 
 	for(let value of tokens) {
 	ul.insertAdjacentHTML('beforeend', `
@@ -45,7 +51,6 @@ async function populateForm() {
 				<div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox">
 				</div>
 				<input type="checkbox" name="Token-ID-${value}" id="Token-ID-1" data-name="Token ID ${value}" value="${value}" style="opacity:0;position:absolute;z-index:-1">
-				<input type="hidden" value="${addr}">
 				<span for="Token-ID-${value}" class="checkbox-label w-form-label">bitcoin bar<span class="label-small-span"><br>Token ID: ${value}</span></span>
 				<div class="logo-wrap">
 					<img src="https://uploads-ssl.webflow.com/60aeba699cbed144e00e4216/61ee2b09334fc99b8a382279_bar-sample%201.png" loading="lazy" alt="" class="image-4" width="48">
